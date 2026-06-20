92f28b1b25834195c69a9fcefa81d9705e353b8d
Ferassehwail <98327438+Ferassehwail@users.noreply.github.com>
2026-06-20 14:41:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
-const CACHE = "quiz-app-v1";
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
+let deferredPrompt;
+const installButton = document.getElementById('installButton');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // منع المتصفح من إظهار الشريط التلقائي
+  e.preventDefault();
+  // تخزين الحدث ليتم استخدامه لاحقاً
+  deferredPrompt = e;
+  // إظهار زر التثبيت الخاص بك
+  installButton.style.display = 'block';
+});
+
+installButton.addEventListener('click', (e) => {
+  // إخفاء الزر
+  installButton.style.display = 'none';
+  // إظهار نافذة التثبيت في المتصفح
+  deferredPrompt.prompt();
+  // انتظار رد المستخدم
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('تم قبول التثبيت');
+    }
+    deferredPrompt = null;
+  });
 });
